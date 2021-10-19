eb5fdb60d6a1e3554baa8cbc49a1cb04482a6017
sbongsu <a_s111@nate.com>
2021-10-19 22:03:44 +0900

    게시물 테스트데이터 10개 자동생성

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -11,21 +11,37 @@ import com.sbs.exam.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
-	private int articleLastId;
+	private int articlesLastId;
 	private List<Article> articles;
 
 	public UsrArticleController() {
+		articlesLastId = 0;
 		articles = new ArrayList<>();
+		makeTestData();
+	}
+
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			int id = articlesLastId + 1;
+			
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			Article article = new Article(id, title, body);
+			articles.add(article);
+			articlesLastId = id;
+		}
+		
 	}
 
 	@RequestMapping("usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		int id = articleLastId + 1;
+		int id = articlesLastId + 1;
 		Article article = new Article(id, title, body);
 
 		articles.add(article);
-		articleLastId++;
+		articlesLastId++;
 		return article;
 	}
 
@@ -35,4 +51,5 @@ public class UsrArticleController {
 
 		return articles;
 	}
+
 }
