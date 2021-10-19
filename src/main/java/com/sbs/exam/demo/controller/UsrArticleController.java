f80c90536e626246283a15c7698a61b594c0fd11
sbongsu <a_s111@nate.com>
2021-10-19 23:26:17 +0900

    setTitle, setBody를 이용하여 게시물 수정 기능 구현

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -11,18 +11,19 @@ import com.sbs.exam.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
-	
-	//인스턴스 변수
+
+	// 인스턴스 변수
 	private int articlesLastId;
 	private List<Article> articles;
 
-	//생성자
+	// 생성자
 	public UsrArticleController() {
 		articlesLastId = 0;
 		articles = new ArrayList<>();
 		makeTestData();
 	}
-	//서비스 메서드 시작
+
+	// 서비스 메서드 시작
 	private void makeTestData() {
 		for (int i = 1; i <= 10; i++) {
 			String title = "제목" + i;
@@ -40,17 +41,19 @@ public class UsrArticleController {
 		articlesLastId = id;
 		return article;
 	}
+
 	private Article getArticle(int id) {
-		for(int i = 0; i < articles.size();i++) {
+		for (int i = 0; i < articles.size(); i++) {
 			Article article = articles.get(i);
-			if(article.getId() == id) {
+			if (article.getId() == id) {
 				return article;
 			}
 		}
 		return null;
 	}
-	//서비스 메서드 끝
-	//액션 메서드 시작
+
+	// 서비스 메서드 끝
+	// 액션 메서드 시작
 	@RequestMapping("usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -66,17 +69,31 @@ public class UsrArticleController {
 
 		return articles;
 	}
-	
+
 	@RequestMapping("usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
 		Article article = getArticle(id);
-		
-		if(article == null) {
+
+		if (article == null) {
 			return id + "번 게시물은 존재하지 않습니다";
 		}
 		articles.remove(article);
 		return id + "번 게시물을 삭제했습니다.";
 	}
-	//액션 메서드 끝
+
+	@RequestMapping("usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물은 존재하지 않습니다";
+		}
+
+		article.setTitle(title);
+		article.setBody(body);
+		return id + "번 게시물을 수정했습니다.";
+	}
+	// 액션 메서드 끝
 }
