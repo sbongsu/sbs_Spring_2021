c05e035169be3164c19f2c9cec675dc6780d34c3
sbongsu <a_s111@nate.com>
2021-10-25 16:43:37 +0900

    SQL 쿼리를 xml 파일로 분리

--- src/main/java/com/sbs/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/sbs/exam/demo/repository/ArticleRepository.java
@@ -13,25 +13,17 @@ import com.sbs.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-	
-	@Insert("INSERT INTO article SET regDate = now(), updateDate = now(), title= #{title}, `body` = #{body}")
-	public void writeArticle(@Param("title") String title,@Param("body") String body);
-	
-	@Select("SELECT * FROM article WHERE id = #{id}")
+
+	public void writeArticle(@Param("title") String title, @Param("body") String body);
+
 	public Article getArticle(@Param("id") int id);
 
-	
-	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(@Param("id") int id);
 
-	@Update("UPDATE article SET updateDate - now(), title = #{title}, `body` = #{body} where id = #{id}")
-	public void modifyArticle(@Param("id") int id,@Param("title") String title,@Param("body") String body);
+	public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 
-
-	@Select("SELECT * FROM article")
 	public List<Article> getArticles();
 
-	@Select("SELECT LAST_INSERT_ID()")
 	public int LastInsertId();
 
 }
