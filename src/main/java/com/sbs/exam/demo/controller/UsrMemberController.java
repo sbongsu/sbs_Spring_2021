d196b12775595b188d61ca47aebbd5014352bfef
sbongsu <a_s111@nate.com>
2021-10-28 16:41:17 +0900

    회원가입시 입력 데이터 유효성 체크

--- src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
@@ -19,7 +19,24 @@ public class UsrMemberController {
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
-
+		if(loginId == null) {
+			return "loginId(을)를 입력해주세요";
+		}
+		if(loginPw == null) {
+			return "loginPw(을)를 입력해주세요";
+		}
+		if(name == null) {
+			return "name(을)를 입력해주세요";
+		}
+		if(nickname == null) {
+			return "nickname(을)를 입력해주세요";
+		}
+		if(cellphoneNo == null) {
+			return "cellphoneNo(을)를 입력해주세요";
+		}
+		if(email == null) {
+			return "email(을)를 입력해주세요";
+		}
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		if (id == -1) {
 			return "사용중인 로그인 아이디입니다.";
