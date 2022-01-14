07250242e20462c04e089d2fa39feb3891b5b098
sbongsu <a_s111@nate.com>
2022-01-14 22:56:03 +0900

    회원정보 수정 전에, 비밀번호 체크시 인증코드 생성

--- src/main/java/com/sbs/exam/demo/service/MemberService.java
+++ src/main/java/com/sbs/exam/demo/service/MemberService.java
@@ -11,9 +11,11 @@ import com.sbs.exam.demo.vo.ResultData;
 public class MemberService {
 
 	private MemberRepository memberRepository;
+	private AttrService attrService;
 	
-	public MemberService(MemberRepository memberRepository) {
+	public MemberService(MemberRepository memberRepository, AttrService attrService) {
 		this.memberRepository = memberRepository;
+		this.attrService = attrService;
 	}
 
 	public ResultData<Integer> join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
@@ -47,6 +49,14 @@ public class MemberService {
 
 		memberRepository.modify(id, nickname, loginPw, email, cellphoneNo);
 		return ResultData.from("S-1", "회원정보를 수정했습니다.");
+	}
+
+	public String genMemberModifyAuthKey(int actorId) {
+		String memberModifyAuthKey = Ut.getTempPassword(10);
+
+		attrService.setValue("member", actorId, "extra", "memberModifyAuthKey", memberModifyAuthKey, Ut.getDateStrLater(60 * 5));
+
+		return memberModifyAuthKey;
 	}
 
 
